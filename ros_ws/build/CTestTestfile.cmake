1d324b99255ae77406e8927e194ecad13c8eb9ea
d-artis <artis.d@northeastern.edu>
2023-04-07 17:14:46 -0400

    Added nuDOG_CHAMP config package

---
+++ ros_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/dartis/NU-Dog/ros_ws/src
+# Build directory: /home/dartis/NU-Dog/ros_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("nuDogv4_config")
