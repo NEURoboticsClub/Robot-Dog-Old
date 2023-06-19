774ddd5b88e22dbe44e82ccd99c60c0f7a12721b
Sherly Hartono <shartono1@gmail.com>
2023-06-18 23:44:13 -0400

    clean up

--- cpu/cpu.py
+++ cpu/cpu.py
@@ -13,15 +13,15 @@ if __name__ == "__main__":
     rospy.init_node('cpu_node')
     pub = rospy.Publisher('cpu_topic', String, queue_size=100)
     sub = rospy.Subscriber('mc_topic', String, callback)
-    rate = rospy.Rate(10) # publishing rate (50 per seconds)
+    rate = rospy.Rate(40) # publishing rate (40 msgs per seconds)
     print("cpu_node started...")
-    # publish
+
+    # publish and increment id at each publish
     id = 1
     while not rospy.is_shutdown():
-        current_time = datetime.now()
-        formatted_time = current_time.strftime("%H:%M:%S")  
+        # current_time = datetime.now()
+        # formatted_time = current_time.strftime("%H:%M:%S")  
         msg = "hello from cpu node! id=" + str(id)
-        print(msg)
         pub.publish(msg)
         id+=1
         rate.sleep()
--- pi/bridge_nodes_thread/cpu_sub_thread.py
+++ pi/bridge_nodes_thread/cpu_sub_thread.py
@@ -53,4 +53,4 @@ if __name__ == "__main__":
         pass
             
             
-    client_socket.close()
+    # client_socket.close()
