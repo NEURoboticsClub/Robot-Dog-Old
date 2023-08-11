81a678c347b5d342503f336230411caa8dc3fa15
Sherly Hartono <sharton1@asu.edu>
2023-08-11 16:30:32 -0400

    publish to mc as soon as we receive joints_pos

--- cpu-catkins/catkin_ws/src/cpu/src/cpu.py
+++ cpu-catkins/catkin_ws/src/cpu/src/cpu.py
@@ -14,9 +14,9 @@ class CPUNode:
         # [[id1, pos1, vel1, torque1], ..., id12, pos12, vel12, torque12]]
         self.mc12_data = queue.Queue() 
 
-        # 2. this will store positions for the 12 mc 
+        # 2. this will store command positions for the 12 mc 
         # [ 0.22, 1.231 , .... ]
-        self.joints_pos_data = queue.Queue()
+        self.joints_cmd_pos_data = queue.Queue()
         self.msg_id = 1
         self.prevErrors = [0] * 12
         self.prevTime = rospy.Time.now().to_sec()
@@ -27,12 +27,12 @@ class CPUNode:
         self.pub = rospy.Publisher('cpu_topic', String, queue_size=100)
 
         # 4. subscribe to mc and joint_states
-        self.sub_mc = rospy.Subscriber('mc_topic', String, self.get_mc_info)
-        self.sub_joint_states = rospy.Subscriber('joint_states', JointState, self.get_joint_states)
+        self.sub_mc = rospy.Subscriber('mc_topic', String, self.get_mc_states)
+        self.sub_joint_states = rospy.Subscriber('joint_states', JointState, self.get_joint_cmd)
 
         print("cpu_node started...")
 
-    def get_mc_info(self, data):
+    def get_mc_states(self, data):
         # 1. grab the data that's just been published
         json_dict = json.loads(data.data)
         msg_id = json_dict.get("id", "No ID found")
@@ -41,29 +41,33 @@ class CPUNode:
         # 2. store it 
         self.mc12_data.put(mc12)
 
-        print("CPU_NODE: id={}, from MC={}".format(msg_id, mc12))
+        # print("CPU_NODE: id={}, from MC={}".format(msg_id, mc12))
 
-    def get_joint_states(self, data):
-        self.time = data.header.stamp.secs
-        self.joints_pos_data.put(data.position)
+    def get_joint_cmd(self, data):
+        self.time = data.header.stamp.to_sec()
 
-    def arrayDiffFinder(self, mcs12_pos, joint_states_pos):
+
+        # Directly call publish_command with the received data
+        self.publish_command(data.position)
+
+    def arrayDiffFinder(self, mcs12_pos, joint_cmd_pos):
         """
         mcs12_pos is array of position of the 12 motors from mc
-        joint_states_pos is array of posisition of the 12 motors from joint_states_pos
+        joint_cmd_pos is array of position of the 12 motors from champ command
         time is the last time we get joint_states postion
         prevTime is the last time this function is called
         """
 
         kp = 0.5
         kd = 0.5
-        errors = [(joint_states_pos[i] - mcs12_pos[i]) for i in range(0, len(mcs12_pos))]
+        # print("CPU_NODE: currPos2={}, cmdPos2={}".format(mcs12_pos[1], joint_cmd_pos))
+        errors = [(joint_cmd_pos[i] - mcs12_pos[i]) for i in range(0, len(mcs12_pos))]
         dError = [((errors[i] - self.prevErrors[i]) / (self.time - self.prevTime)) for i in range(0,len(errors))] 	
         vels = [kp * errors[i]+ kd * dError[i] for i in range(0,len(errors))]
         
         return vels, self.time, errors
 
-    def publish_command(self):
+    def publish_command(self, joints_pos_cmd):
         mc12_command = None
 
         # 1. if this is the first command, just hard code or use the first command from joint_states?
@@ -72,18 +76,23 @@ class CPUNode:
 
         # 2. motor already moving, do some compute
         else:
-            # don't publish any command if the last time we publish is the same second
+            # - don't publish any command if the last time we publish is not more than 1 second
             if self.prevTime == self.time:
                 return
-            # - grab the last mc states and the command from joints
+            
+            # - grab the last mc states position and the command from joints
             mc12_last = [ datas[1] for datas in self.mc12_data.get()]
-            joints_last = self.joints_pos_data.get()
+
+            # log if this is a real command from teleop
+            # epsilon = 1e-10
+            # if abs(joints_pos_cmd[0] - (-7.351371067443324e-08)) > epsilon:
+            #     print("CPU_NODE: publish on pos={}".format(joints_pos_cmd))
+
 
             # - do some compute
-            # print("CPU_NODE: currT={}, prevT={}".format(self.time, self.prevTime))
-            vels, self.prevTime, self.prevErrors = self.arrayDiffFinder(mc12_last, joints_last)
-            
-            # - assign to mc12 command
+            vels, self.prevTime, self.prevErrors = self.arrayDiffFinder(mc12_last, joints_pos_cmd)
+            print("CPU_NODE: vels={}".format(vels))
+            # - assign tthe vel to mc12 command
             mc12_command= [[id, float('nan'),vels[id-1], 3.0] for id in range(1,13)]
 
         # send the 12 of them
@@ -93,10 +102,8 @@ class CPUNode:
 
 
     def run(self):
-        rate = rospy.Rate(40) # publishing rate (40 per second)
-        while not rospy.is_shutdown():
-            self.publish_command()
-            rate.sleep()
+        # rate = rospy.Rate(40) # publishing rate (40 per second)
+        rospy.spin()
 
 if __name__ == "__main__":
     node = CPUNode()
