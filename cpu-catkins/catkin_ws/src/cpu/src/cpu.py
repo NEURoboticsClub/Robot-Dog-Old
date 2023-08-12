5e275471f62d1c7981d51c231517702d783d523f
Sherly Hartono <sharton1@asu.edu>
2023-08-11 20:25:38 -0400

    sending command responsive

--- cpu-catkins/catkin_ws/src/cpu/src/cpu.py
+++ cpu-catkins/catkin_ws/src/cpu/src/cpu.py
@@ -10,29 +10,37 @@ from sensor_msgs.msg import JointState
 class CPUNode:
     def __init__(self):
         rospy.init_node('cpu_node')
-        # 1. each element is an array of 12 elements:
+        # 1. to store mc12 current states
+        # each element is an array of 12 elements:
         # [[id1, pos1, vel1, torque1], ..., id12, pos12, vel12, torque12]]
         self.mc12_data = queue.Queue() 
+        self.motor_started = False
 
-        # 2. this will store command positions for the 12 mc 
+        # 2. to store mc12 expected states or command (from joint_states)
         # [ 0.22, 1.231 , .... ]
         self.joints_cmd_pos_data = queue.Queue()
-        self.msg_id = 1
         self.prevErrors = [0] * 12
+
+        # 3. time and id tracker
         self.prevTime = rospy.Time.now().to_sec()
         self.time = rospy.Time.now().to_sec()
+        self.msg_id = 1
 
         
-        # 3. set publisher for the command from cpu
+        # 4. set publisher for the command from cpu
         self.pub = rospy.Publisher('cpu_topic', String, queue_size=100)
 
-        # 4. subscribe to mc and joint_states
+        # 5. subscribe to mc and joint_states
         self.sub_mc = rospy.Subscriber('mc_topic', String, self.get_mc_states)
         self.sub_joint_states = rospy.Subscriber('joint_states', JointState, self.get_joint_cmd)
 
         print("cpu_node started...")
 
     def get_mc_states(self, data):
+
+        if not self.motor_started:
+            self.motor_started = True
+
         # 1. grab the data that's just been published
         json_dict = json.loads(data.data)
         msg_id = json_dict.get("id", "No ID found")
@@ -46,10 +54,54 @@ class CPUNode:
     def get_joint_cmd(self, data):
         self.time = data.header.stamp.to_sec()
 
-
-        # Directly call publish_command with the received data
+        # Directly call publish_command with the received position command
         self.publish_command(data.position)
 
+
+    def publish_command(self, joints_pos_cmd):
+
+        # log if this is a real command from teleop
+        # epsilon = 1e-10
+        # if abs(joints_pos_cmd[0] - (-7.351371067443324e-08)) > epsilon:
+        #     print("CPU_NODE: publish on pos={}".format(joints_pos_cmd))
+
+
+        mc12_command = None
+        # case 1. if mc12 not yet started, keep sending the first hard coded command
+        # it will only set to true once we receive the first mc parsed result
+        if not self.motor_started:
+            mc12_command = [[mcid, float('nan'), 2.0, 1.0] for mcid in range(1, 13)]
+            
+
+        # case 2. motor already moving
+        else:
+            # 1. don't publish any command if no data from mc 
+            # - question: should we send previous command?
+            if self.mc12_data.empty():
+                return
+            
+            # 2. grab the last mc states position and the command from joints
+            mc12_last = [ datas[1] for datas in self.mc12_data.get()]
+
+            # 3. do some compute
+            vels, self.prevTime, self.prevErrors = self.arrayDiffFinder(mc12_last, joints_pos_cmd)
+            
+            # 4. assign the output velocity
+            mc12_command= [[id, float('nan'),vels[id-1], 1.0] for id in range(1,13)]
+            
+            # mc12_command= [[id, float('nan'), 2.0, 1.0] for id in range(1,13)]
+
+
+        print("CPU_NODE: id={}, 1st cmd2={}".format(self.msg_id, mc12_command[1]))
+            
+        
+        # 5. send the 12 motor commands to mc
+        json_tosend = json.dumps({"id": self.msg_id, "mc12": mc12_command})
+        self.pub.publish(json_tosend)
+        self.msg_id += 1
+
+
+    # Helper function for publish_command
     def arrayDiffFinder(self, mcs12_pos, joint_cmd_pos):
         """
         mcs12_pos is array of position of the 12 motors from mc
@@ -67,42 +119,9 @@ class CPUNode:
         
         return vels, self.time, errors
 
-    def publish_command(self, joints_pos_cmd):
-        mc12_command = None
-
-        # 1. if this is the first command, just hard code or use the first command from joint_states?
-        if self.mc12_data.empty():
-            mc12_command = [[mcid, float('nan'), 2.0, 1.0] for mcid in range(1, 13)]
-
-        # 2. motor already moving, do some compute
-        else:
-            # - don't publish any command if the last time we publish is not more than 1 second
-            if self.prevTime == self.time:
-                return
-            
-            # - grab the last mc states position and the command from joints
-            mc12_last = [ datas[1] for datas in self.mc12_data.get()]
-
-            # log if this is a real command from teleop
-            # epsilon = 1e-10
-            # if abs(joints_pos_cmd[0] - (-7.351371067443324e-08)) > epsilon:
-            #     print("CPU_NODE: publish on pos={}".format(joints_pos_cmd))
-
-
-            # - do some compute
-            vels, self.prevTime, self.prevErrors = self.arrayDiffFinder(mc12_last, joints_pos_cmd)
-            print("CPU_NODE: vels={}".format(vels))
-            # - assign tthe vel to mc12 command
-            mc12_command= [[id, float('nan'),vels[id-1], 3.0] for id in range(1,13)]
-
-        # send the 12 of them
-        json_tosend = json.dumps({"id": self.msg_id, "mc12": mc12_command})
-        self.pub.publish(json_tosend)
-        self.msg_id += 1
 
 
     def run(self):
-        # rate = rospy.Rate(40) # publishing rate (40 per second)
         rospy.spin()
 
 if __name__ == "__main__":
